c514e99859f150342fdf7c170a2781e6ce382ebf
João Pedro FG <jpedrofgomes@gmail.com>
2025-05-14 15:11:30 -0300

    adicionando arquivos de exemplos de aula

---
+++ Codigos - Aulas/Aula2Tidyverse.R
@@ -0,0 +1,18 @@
+# Pra instalar, 
+library(tidyverse)
+library(nycflights13)
+base_agrupada <- flights %>%
+  filter(dest == "IAH") %>%
+  group_by(year, month, day) %>%
+  summarize(
+    arr_delay = mean(arr_delay, na.rm = TRUE)
+  )
+
+flights <- flights |>
+  group_by(year, month, day) |>
+  mutate(arr_delay_day = mean(arr_delay, na.rm = TRUE)) |>
+  ungroup()
+
+plot <- flights |>
+  filter(dest == "IAH") |>
+  ggplot(aes(x=arr_delay, y=dep_delay)) + geom_point(colour = "#45ff66")
